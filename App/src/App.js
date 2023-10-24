99969188355860aa5f567c9b4175d63ed7fadf55
Kayseruss <86257434+Kayseruss@users.noreply.github.com>
2023-10-24 17:29:37 +0000

    pronto

---
+++ App/src/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+
+{/*import Comp, { Comp2, Comp3 } from './Components/Multiplos';
+import Primeiro from './Components/Primeiro';
+import MixMax from './Components/MixMax';
+import Aleatorio from './Components/Aleatorio';
+import Botao from './Components/Botao';
+import Titulo from './Components/Titulo';*/}
+
+//import Contador from './Components/Contador';
+
+import Pai from './Components/direta/Pai'
+
+export default function App() { 
+  return (
+    <SafeAreaView style={styles.container}>
+      
+      <Pai/>
+      
+      {/*<Titulo principal="Titulo" secundario="Subtitulo"/>
+      <Contador inicial={100} passo={2}/>
+      <Contador/>
+      <Botao/>
+      <Aleatorio min={1} max={60}/>
+    <Aleatorio min={1} max={60}/>
+    <Aleatorio min={1} max={60}/>
+    <Aleatorio min={1} max={60}/>
+    <Aleatorio min={1} max={60}/>
+    <Aleatorio min={1} max={60}/>
+      <MixMax min={2} max={20 }/>
+      <MixMax min={15} max={65 }/>
+      
+      <Comp />
+      <Comp2 />
+      <Comp3 />
+  <Primeiro />*/}
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
